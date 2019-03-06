728c1ac29d2cd48f5043d6904f9f6cd7c20a342e
m0n4d1 <m0n4d1000@gmail.com>
2019-03-06 15:58:35 +1000

    refactoring menu system

--- main.rb
+++ main.rb
@@ -1,166 +1,106 @@
-menu_main = {
-  name: "Main menu",
-  description: "some details",
-  options: [
-    "go back",
-    "register habits",
-    "Diary",
-    "metrics",
-    "help"
-  ]
-}
-
-menu_habits = {
-  name: "Habits menu",
-  description: "some details",
-  options: [
-    "go back",
-    "create habit",
-    "edit habit",
-    "remove habit",
-    "list habits"
-  ]
-}
-
-menu_diary = {
-  name: "Diary menu",
-  description: "some details",
-  options: [
-    "go back",
-    "list habbits",
-    "roster daily",
-    "calandar"
-  ]
-}
-
-menu_metrics = {
-  name: "Metrics menu",
-  description: "some details",
-  options: [
-    "go back",
-    "option 1",
-    "option 2",
-    "option 3"
-  ]
-}
-
-
-
-# "habit tracker"
-
-#INIT
-#------------------
-#cross-platform clear terminal
-clear = Gem.win_platform? ? (system "cls") : (system "clear")
-#------------------
-
-def menu_details(menu)
-
-  system "clear"
-  print "Welcome to the #{menu[:name]}.\n"
-  print "Description: #{menu[:description]}\n"
-  menu[:options].each_with_index do |option,i|
-    print "#{i}: #{option}, "
+class MenuController
+  attr_reader :menus
+  
+  def initialize
+    @menus = {
+      main: {
+        name: "Main menu",
+        description: "some details",
+        options: [
+          {
+            label: "go back",
+            action: 0
+          },
+          {
+            label:"register habits",
+            action: open_menu(@menus[:habits])
+          },
+          {
+            label: "Diary",
+            action: 0
+          },
+          {
+            label: "metrics",
+            action: 0
+          },
+          {
+            label: "help",
+            action: 0
+          }
+        ]
+      },
+      habits: {
+        name: "Habits menu",
+        description: "some details",
+        options: [
+          "go back",
+          "create habit",
+          "edit habit",
+          "remove habit",
+          "list habits"
+        ]
+      },
+      diary: {
+        name: "Diary menu",
+        description: "some details",
+        options: [
+          "go back",
+          "list habbits",
+          "roster daily",
+          "calandar"
+        ]
+      },
+      metrics: {
+        name: "Metrics menu",
+        description: "some details",
+        options: [
+          "go back",
+          "option 1",
+          "option 2",
+          "option 3"
+        ]
+      }
+    }
   end
-  print "\n"
-  print "enter a number to make a selection\n"
 
-end
-
-#main loop
-loop do
-  #print details
-  #------------------
-  menu_details(menu_main)
-  #------------------
-
-  #handle input
-  #------------------
-  input = gets.strip.chomp.to_i
-  case input 
-    when 1
+  def menu_details(menu)
+    system "clear"
+    print "Welcome to the #{menu[:name]}.\n"
+    print "Description: #{menu[:description]}\n"
+    menu[:options].each_with_index do |option,i|
+      print "#{i}: #{option[:label]}, "
+    end
+    print "\n"
+    print "enter a number to make a selection\n"
+  end
+  
+  def open_menu(menu)
+    loop do
+  
+      # menu_details(menu)
       
-      loop do
-        #Print details
-        #------------------
-        menu_details(menu_habits)
-        #------------------
-
-
-        #handle input
-        #------------------
-        input = gets.strip.chomp.to_i
-        
-        case input
-          when 0
-            break
-          else
-            
-        end
-        #------------------
-      end
-
-    when 2
-
-      
-      loop do
-        #Print details
-        #------------------
-        clear
-        menu_details(menu_diary)
-        #------------------
-
-        #handle input
-        #------------------
-
-        input = gets.strip.chomp.to_i
-        case input 
-          when 0
-            break
-          when 1
-
-          when 2
-
-          when 3
-
-          
-          else
-            
-        end
-        #------------------
+      input = gets.strip.chomp.to_i
+      if input == 0
+        break
+      elsif input <= menu[:options].length
+        menu[:options][input][:action]
+      else
         
       end
-
-
-    when 3
-
-      loop do
-        #Print details
-        #------------------
-        menu_details(menu_metrics)
-        #------------------
-
-        #handle input
-        #------------------
-
-        input = gets.strip.chomp.to_i
-        case input 
-          when 1
-
-          when 2
-
-          when 3
-
-          when 0
-            exit = true
-          
-          else
-            break
-        end
-        #------------------
-      end
-    else
-      break
+    end
+  
   end
-  #------------------
+
+  def init_menu_system
+    open_menu(@menus[:main])
+  end
 end
+
+menu_controller = MenuController.new
+
+
+menu_controller.init_menu_system
+
+
+
+
